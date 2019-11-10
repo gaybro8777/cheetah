2497651482667780c984f08402cd6cdfea7dde60
niceyeti <jesse_waite@hotmail.com>
2019-11-09 22:20:51 -0800

    Topical cheetah data analysis in pandas: plot topical cheetah values aggregated by week

--- src/scripts/cheetah_munging.py
+++ src/scripts/cheetah_munging.py
@@ -10,73 +10,71 @@ import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def queryTopic(df, topics):
+	"""
+	@df: The stories_election_csv data frame
+	@topics: Topic terms by which to filter on 'title'.
+	"""
+	return df[ df['title'].str.contains("|".join(topics), case=False) ]
+
 def convertPublishDate(df):
-	df['publish_date'] = pd.to_datetime(df['publish_date'], infer_datetime_format=True)
+	# converts publish_date string values in format "2015-12-28 07:00:00" to datetime objects
+	df['publish_date'] = pd.to_datetime(df['publish_date'], format="%Y-%m-%d %H:%M:%S", errors='coerce')
 	return df
 
 def loadData():
 	#TODO: only read columns of interest. This reads tons on unused data.
-	dataPath = "../../data/stories_election_web_cheetofied_test.csv"
+	dataPath = "../../data/stories_election_web_cheetofied.csv"
 	print("Loading dataset from "+dataPath)
 	df = pd.read_csv(dataPath, header=0)
 	return df
 
+def filterByDateTimeRange(df, minDt, maxDt):
+	return df[ (df['publish_date'] >= minDt) & (df['publish_date'] <= maxDt) ]
+
+# groupby isoweek+year, sum cheetah values per week
+def groupByWeekYear(df):
+	return df.groupby(pd.Grouper(key='publish_date', freq="W-MON"))
+
+def sumAndPlotCheetahValues(grp):
+	summed = grp['cheetah'].sum()
+	print("SUMMED: ", summed)
+	summed.plot()
+
+def filterCheetahNans(df):
+	# Missing cheetah values (e.g. not headline/language data) are stored as NaN. This filters them.
+	return df[ df['cheetah'].notnull() ]
+
+def plotTopicalCheetahTimeSeries(df, topicLists, minDt, maxDt):
+	# Plot topical cheetah values as time series
+	#@df: NOTE: Must have publish_date values converted to datetime before calling!
+	for topicList in topicLists:
+		df = filterByDateTimeRange(df, minDt, maxDt)
+		tf = queryTopic(df, topicList)
+		tf = filterCheetahNans(tf)
+		grp = groupByWeekYear(tf)
+		s = sumAndPlotCheetahValues(grp)
+	plt.show()
+
+def filterBySource(df, urls):
+	print("Getting by source per urls: ", urls)
+	return df[ df['media_url'].str.contains("|".join(urls), case=False) ]
+
+# load harvard data
 df = loadData()
-# Filter by org
-mediaUrl = "cnn.com"
-print("Filtering by media_url: "+mediaUrl)
-df = df[ df['media_url'].str.contains(mediaUrl, case=False) ]#.to_frame()
-# Filter by subject in title
-topic = "clinton"
-print(str(type(df)))
-print("cols: ", df.columns)
-print("Filtering by topic: "+topic)
-t1 = df[ df['title'].str.contains(topic, case=False) ]
-topic = "trump"
-print("Filtering by topic: "+topic)
-t2 = df[ df['title'].str.contains(topic, case=False) ]
-#filter cheetah nans
-t1 = t1[ t1['cheetah'].notnull() ]
-t2 = t2[ t2['cheetah'].notnull() ]
-
-print(t1)
-print(type(t1))
-t1.hist(column="cheetah")
-t2.hist(column="cheetah")
-plt.show()
-
-#Awesome time series resource: https://towardsdatascience.com/basic-time-series-manipulation-with-pandas-4432afee64ea
-t1 = convertPublishDate(t1)
-t2 = convertPublishDate(t2)
-
+# filter by org
+df = filterBySource(df, ["cnn.com"])
+# get topics, group by week, and plot aggregate cheetah values by week
+topicLists = [["clinton","hillary"], ["trump","donald"] ]
+minDt = datetime.datetime(year=2015, month=1, day=1)
+maxDt = datetime.datetime(year=2016, month=12, day=31)
+df = convertPublishDate(df)
+plotTopicalCheetahTimeSeries(df, topicLists, minDt, maxDt)
 
 
 
 
 """
-def binByWeek(recs):
-
-
-def plotSubset()
-	""
-
-	""
-
-def orgQuery(df, orgUrls, topics=None):
-	""
-	Query the harvard csv dataframe by media_url column, with optional case insensitive topical filtering.
-
-	@df: The harvard csv data loaded into a dataframe
-	@orgUrls: A list of urls describing some organization or group of them, against which to match in the media_url column as a substring.
-		Example: 'give me all records for 'foxnews.com' and 'foxbusiness.com' by matching these substrings in the media_url column'
-	@topics: Optional set of substring to filter records by (case insensitive), over @filterCols
-	""
-	
-
-
-
-
-
 def 
 	srcs = [
 		["cnn.com"],
